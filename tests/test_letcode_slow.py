f149698d4e44e1c67efcb1c348451e5f49b9d005
Lokankara <poliyakpavlo@gmail.com>
2024-06-10 12:57:03 +0300

    add input test

--- tests/test_letcode_slow.py
+++ tests/test_letcode_slow.py
@@ -10,5 +10,17 @@ def test_radiobutton(page: Page):
     page.locator("#two").check()
     # page.get_by_label("Remember me").uncheck()
     radio_loct = page.locator("#two")
-    # page.pause()
     expect(radio_loct).to_be_checked()
+
+
+def test_edit_button(page: Page):
+    page.goto('https://letcode.in/edit')
+    input = page.locator("//input[@id='fullName']")
+    expect(input).to_be_editable()
+    input.fill("Test")
+    second_input = page.locator("#join")
+    expect(second_input).to_be_visible()
+    expect(second_input).not_to_be_empty()
+    text = second_input.inner_html()
+    print(text)
+    page.pause()
