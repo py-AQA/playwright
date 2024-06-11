71fe85bfa5115a3cf0dc064687e756d82dea7399
Grom-Zadira85 <Trall18111985@gmail.com>
2024-06-11 20:26:46 +0300

    add checkbox test

---
+++ luma/tests/checkbox.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import Page, expect
+
+
+def test_checkbox(page: Page):
+    page.goto('https://zimaev.github.io/checks-radios/')
+    # page.pause()
+    page.locator("text=Default checkbox").check()
+    page.locator("text=Checked checkbox").check()
+    page.locator("text=Default radio").check()
+    page.locator("text=Default checked radio").check()
+    page.locator("text=Checked switch checkbox input").check()
+
+
+def test_checkbox2(page):
+    page.goto('https://zimaev.github.io/checks-radios/')
+    page.locator("text=Default checkbox").click()
+    page.locator("text=Checked checkbox").click()
+    page.locator("text=Default radio").click()
+    page.locator("text=Default checked radio").click()
+    page.locator("text=Checked switch checkbox input").click()
