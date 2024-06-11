77672a74d445d850da7ac96fe2c3f730c2ea884c
Grom-Zadira85 <Trall18111985@gmail.com>
2024-06-11 16:50:14 +0300

    Refactor tests

--- luma/tests/test_mini_cart_from_main_page.py
+++ luma/tests/test_mini_cart_from_main_page.py
@@ -1,10 +1,7 @@
-import time
-
 from playwright.sync_api import Page
 from playwright.sync_api import expect
 
 from luma.pages import mini_cart
-from luma.pages.locators import ProductLocators as PL, ShoppingCart as SC
 
 
 def test_redirection_magento_store_notes(page: Page):
@@ -36,39 +33,3 @@ def test_add_item(page: Page):
     # expect(qty_counter).to_have_text("1")
 
 
-
-
-# def test_go_to_mini_cart(page: Page):
-#     page.goto(mini_cart.main_page_link)
-#     # page.pause()
-#     mini_cart.add_item(page, name_item="Argus All-Weather Tank", size="M", color="Gray")
-    # page.get_by_role("link", name=" My Cart 1 1 items").click()
-
-# def test_color_and_clickability_of_view_and_edit_cart_link_in_the_mini_cart():
-#     go_to_mini_cart()
-#     # mini_cart = BasketPage(browser, main_page_link)
-#     mini_cart.check_color_of_view_and_edit_cart_link_in_the_mini_cart()
-#     mini_cart.check_clickability_of_view_and_edit_cart_link_in_the_mini_cart()
-#
-#
-# def test_checking_the_link_opens_the_cart_page():
-#     go_to_mini_cart()
-#     mini_cart.checking_the_link_opens_checkout_cart_page()
-#
-#
-# def test_size_color_and_product_name_are_correct():
-#     go_to_mini_cart()
-#     mini_cart.checking_the_size_color_and_product_name_are_correct("M", "Gray", "Argus All-Weather Tank")
-#
-#
-# def test_checking_present_price_item_and_cart_subtotal_in_the_mini_cart():
-#     go_to_mini_cart()
-#     mini_cart.checking_present_price_item_and_cart_subtotal_in_the_mini_cart("$22.00", "$22.00")
-#
-#
-# def test_change_quantity_of_an_item_and_changes_price_in_cart_subtotal_mini_cart():
-#     go_to_mini_cart()
-#     mini_cart.should_be_success_message("You added Argus All-Weather Tank to your shopping cart.")
-#     mini_cart.change_qty("7")
-#     mini_cart.should_be_quantity_change("7")
-#     mini_cart.should_be_change_subtotal("$22.00", "$154.00")
