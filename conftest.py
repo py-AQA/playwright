2b381033635ccf108d352f73a0880fc420a90ea8
sveta <sve3363@gmail.com>
2024-06-12 12:29:26 +0300

    test_upload_file

--- conftest.py
+++ conftest.py
@@ -4,9 +4,9 @@ import pytest
 @pytest.fixture(scope="session")
 def browser_context_args():
     return {
-        "record_video_dir": "videos/",
-        "record_video_size": {"width": 640, "height": 480},
-        "viewport": {"width": 800, "height": 600}
+        # "record_video_dir": "videos/",
+        # "record_video_size": {"width": 640, "height": 480},
+        "viewport": {"width": 800, "height": 800}
         # "viewport": {
         #     "width": 1920,
         #     "height": 1080,
