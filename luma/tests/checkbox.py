d6e16d39469a19e2cd8acfe7b21182252fd1cac1
Grom-Zadira85 <Trall18111985@gmail.com>
2024-06-12 08:38:02 +0300

    add expect for checkbox test

--- luma/tests/checkbox.py
+++ luma/tests/checkbox.py
@@ -18,3 +18,27 @@ def test_checkbox2(page):
     page.locator("text=Default radio").click()
     page.locator("text=Default checked radio").click()
     page.locator("text=Checked switch checkbox input").click()
+
+
+def test_checkbox_checking(page: Page):
+    page.goto('https://zimaev.github.io/checks-radios/')
+    # page.pause()
+    page.locator('#flexCheckDefault').check()
+    first = page.locator('#flexCheckDefault')
+    expect(first).to_be_checked()
+
+    page.locator("text=Checked checkbox").check()
+    second = page.locator("text=Checked checkbox")
+    expect(second).to_be_checked()
+
+    page.locator("text=Default radio").click()
+    third = page.locator("text=Default radio")
+    expect(third).to_be_checked()
+
+    page.locator("text=Default checked radio").click()
+    fourth = page.locator("text=Default checked radio")
+    expect(fourth).to_be_checked()
+
+    page.locator("text=Checked switch checkbox input").click()
+    switch = page.locator("text=Checked switch checkbox input")
+    expect(switch).to_be_checked()
