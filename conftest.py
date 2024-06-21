7f38846ab74d414e344f0dfcf8c6c7aad9267ba5
sveta <sve3363@gmail.com>
2024-06-21 12:38:04 +0300

    test_main_setting

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def browser_context_args():
     return {
         # "record_video_dir": "videos/",
         # "record_video_size": {"width": 640, "height": 480},
-        "viewport": {"width": 800, "height": 800}
+        "viewport": {"width": 1280, "height": 1024}
         # "viewport": {
         #     "width": 1920,
         #     "height": 1080,
