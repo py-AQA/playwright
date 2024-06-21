b4542df09624d5bc5cf28c44c15e18c6ce98d8b8
Konstantin <161002599+Grom-Zadira85@users.noreply.github.com>
2024-06-21 11:57:32 +0300

    add  test_reminder (#43)

--- tests/test_apod_web.py
+++ tests/test_apod_web.py
@@ -19,3 +19,30 @@ def test_timezone_select(page_my: Page):
 def test_bot_messages(page_my: Page):
     page_my.goto("https://apod-dev-d.osora.ru/settings/messages")
     expect(page_my.locator("div#__next nav").first).to_contain_text("СотрудникиНастройкиАналитика")
+
+
+def test_reminder(page_my: Page):
+    page = page_my
+    page.goto("https://apod-dev-d.osora.ru/employees/one/timesheet")
+    page.get_by_text("Напоминания").click()
+    page.get_by_text("+ добавить напоминание").click()
+
+
+    page.get_by_placeholder("Комментарий").last.click()
+    page.get_by_placeholder("Комментарий").last.fill("time")
+
+    page.get_by_text("Зациклить").last.click()
+
+    page.get_by_placeholder("Выберите дату").last.click()
+    page.get_by_role("button", name="1 июня 2024 г.", exact=True).first.click()
+    # page.get_by_placeholder("Выберите дату").nth(2).click()
+    page.locator("li").filter(has_text="Зациклить«‹июнь 2024").locator("path").first.click()
+
+    page.get_by_text("Выберите время").last.click()
+    page.get_by_text("04:30").click()
+
+    page.get_by_text("Сохранить").click()
+    # page.pause()
+    expect(page.locator('[testid="alertTitle"]')).to_have_text("Успех")
+    page.get_by_text("Принять").click()
+
