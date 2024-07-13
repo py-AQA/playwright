e2d972c1fdd3e1f6d62433776998f19eec684532
Grom-Zadira85 <Trall18111985@gmail.com>
2024-07-13 11:15:37 +0300

    draft tet fail

--- luma/bot/test_my_bot.py
+++ luma/bot/test_my_bot.py
@@ -1,15 +1,20 @@
+import asyncio
 import os
 import time
 
+import pytest
+from dotenv import load_dotenv
+from pytest import mark
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from dotenv import load_dotenv
+from telethon.tl.custom.message import Message
 
 load_dotenv()
 
 api_id = os.getenv("APP_ID")
 api_hash = os.getenv("APP_HASH")
 telethon_session = os.getenv("SESSION_NAME")
+
 
 # нужен если убрать мой конфтест файл, настройки из глобально-группового конфтеста не передаются
 # def client() -> TelegramClient:
@@ -19,7 +24,7 @@ telethon_session = os.getenv("SESSION_NAME")
 #     :rtype: TelegramClient
 #     """
 #     client = TelegramClient(
-#         StringSession(telethon_session), api_id, api_hash
+#         StringSession(telethon_session), int(api_id), api_hash
 #     )
 #
 #     yield client
@@ -27,14 +32,30 @@ telethon_session = os.getenv("SESSION_NAME")
 
 def test_bot_say_hi(client: TelegramClient):
     """ Посылаем сообщение нашему боту и проверяем что он отвечает нам """
-    client = TelegramClient(StringSession(telethon_session), api_id, api_hash)
+    client = TelegramClient(StringSession(telethon_session), int(api_id), api_hash)
     client.connect()
 
     # Используется conversation из telethon для того что бы следить за диалогом с ботом
     # with client.conversation("@gromamicon_bot", timeout=5) as conv:
     client.send_message("@gromamicon_bot", "/start")
-    time.sleep(1)
-    resp = client.get_messages("@gromamicon_bot")
-    #     resp = client._get_response_message("/start", "HI", "")
-    print(resp)
+    # time.sleep(1)
+    # resp = client.get_messages("@gromamicon_bot")
+    # #     resp = client._get_response_message("/start", "HI", "")
+    # print(resp)
     # assert resp.text == "Hi"
+
+
+@mark.asyncio
+async def test_bot_send_message(client: TelegramClient):
+    with client.conversation("@gromamicon_bot", timeout=5) as conv:
+        await client.connect()
+        await conv.send_message("/start")
+        resp: Message = await conv.get_response()
+        # resp = await conv.get_response()
+        print(resp)
+        # assert resp.text == "Hi"
+
+
+
+
+
