f4c40cec023b04df53b09982e042edd9963bcde9
Grom-Zadira85 <Trall18111985@gmail.com>
2024-07-13 11:14:55 +0300

    added chat feature

--- luma/bot/my_sesion1.py
+++ luma/bot/my_sesion1.py
@@ -9,7 +9,7 @@ api_id = os.getenv('APP_ID')
 api_hash = os.getenv('APP_HASH')
 session = os.getenv("SESSION_NAME")
 
-with TelegramClient(StringSession(session), api_id, api_hash) as client:
+with TelegramClient(StringSession(session), int(api_id), api_hash) as client:
     print("Your session string is:", client.session.save())
     print(client.get_me())
 
@@ -18,3 +18,5 @@ async def main():
     # Выводим в консоль данные о текущем пользователе, для проверки
     me = await client.get_me()
     print(me.stringify())
+
+    await client.run_until_disconnected()
