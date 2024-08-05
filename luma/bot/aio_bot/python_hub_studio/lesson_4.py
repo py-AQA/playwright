a6ffcbf30fc357f5123e48448c146498e2ae7dc5
Konstantin <161002599+Grom-Zadira85@users.noreply.github.com>
2024-08-05 11:55:37 +0300

    Aio (#94)
    * correct start test
    
    * draft
    
    * draft

--- luma/bot/aio_bot/python_hub_studio/lesson_4.py
+++ luma/bot/aio_bot/python_hub_studio/lesson_4.py
@@ -1,22 +1,24 @@
-
 import asyncio
 import os
 from typing import Final
 
 from aiogram import Bot, Dispatcher, types
-from dotenv import load_dotenv
+from aiogram.client.default import DefaultBotProperties
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
+from aiogram.enums import ParseMode
 from luma.bot.aio_bot.python_hub_studio.bot_cmd_list_les_3 import private
 from luma.bot.aio_bot.python_hub_studio.handlers.user_group import user_group_router
-from luma.bot.aio_bot.python_hub_studio.handlers.user_private_for_lesson_3 import user_private_router
-
+from luma.bot.aio_bot.python_hub_studio.handlers.user_private_4 import user_private_router
 
 load_dotenv()
 bot_token = os.getenv("TOKEN")
 BOT_USERNAME: Final = '@gromamicon_bot'
 print(bot_token)
 
-bot = Bot(bot_token)
+bot = Bot(bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
 dp = Dispatcher()
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
@@ -30,9 +32,17 @@ async def main():
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOW_UPDATES)
 
+
 asyncio.run(main())
 
+""" 
+F.text.startswith("show") фильтр теста  - тест начинается с слова "show"
+F.text.endswith("example")) - тест заканчивается  словом  "example"
 
+and_f(F.text.startswith("show"), F.text.endswith("example")) - объединение условий  and_f
 
-""" Смотри 
+or_f(F.text(text="hi"), CommandStart()) - запуск по команде ИЛИ по тексту
+
+@user_private_router.message(~(F.text.lower().contains ("варианты доставки")))
+будет срабатывать при всех условиях, кроме "варианты доставки"
 """
